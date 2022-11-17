7be04a915dad6aff571afc4b38992310934a7b2f
facuolivamar <facundoolivam@gmail.com>
2022-11-17 12:06:02 -0300

    allowed hosts + csrf

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,8 +25,20 @@ SECRET_KEY = 'django-insecure-5t3xxk2(+m9hz&iw%&r4q^-ip3#(72qv_53%&gb7^e*cki(!%=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+## For example, for a site URL at 'web-production-3640.up.railway.app'
+## (replace the string below with your own site URL):
+ALLOWED_HOSTS = ['web-production-3640.up.railway.app', '0.0.0.0'] #http://0.0.0.0:6128/
 
+# During development, you can instead set just the base URL
+# (you might decide to change the site a few times).
+# ALLOWED_HOSTS = ['.railway.com','127.0.0.1']
+
+## For example, for a site URL is at 'web-production-3640.up.railway.app'
+## (replace the string below with your own site URL):
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app']
+
+# During development/for this tutorial you can instead set just the base URL
+# CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 
 # Application definition
 
